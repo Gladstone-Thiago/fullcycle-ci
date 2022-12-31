3fb29a2f7d3d38a7bf1b0f9dfb5281f399531bc6
Thiago Gladstone <thiagogladstone@hotmail.com>
2022-12-31 00:13:25 -0300

    fix:

--- math_test.go
+++ math_test.go
@@ -4,7 +4,7 @@ import "testing"
 
 func TestSum(t *testing.T) {
 
-	total := sum(15, 15)
+	total := Sum(15, 15)
 
 	if total != 30 {
 		t.Errorf("Resultado da some é inválido: Resultado %d. Esperado: %d", total, 30)
